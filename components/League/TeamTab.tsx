d1815007b30f88a4fa9ee7d5658c09efe18c57f1
unknown <hunchographics@gmail.com>
2022-11-20 19:51:41 +0300

    fantasy

--- components/League/TeamTab.tsx
+++ components/League/TeamTab.tsx
@@ -8,7 +8,7 @@ const TeamTab = ({ league, participant, wallets, position }: { league: any, part
   return (
     <div className={`${L.container} mb-1`}>
     <div className={`${L.resultsRow}`}>
-        <span className="text-sm">{position}</span>  <span className="text-sm">{participant.username}</span> <span className="text-sm">{participant.points}</span> <span className="text-sm">${wallets.filter((wallet:any) => wallet.userId === participant.userId).map((wallet:any) => Intl.NumberFormat('en-US').format(wallet.credits))
+        <span className="text-sm">{position}</span>  <span className="text-sm">{participant.fantasyname}</span> <span className="text-sm">{participant.points}</span> <span className="text-sm">${wallets.filter((wallet:any) => wallet.userId === participant.userId).map((wallet:any) => Intl.NumberFormat('en-US').format(wallet.credits))
         }</span> <a href={`/participant/${league.name}/${participant.fantasyname}/Overview`} className="outline outline-[#ff921b] px-3 text-sm py-0.5 rounded-xl uppercase" >View</a></div></div>)
 
 }
